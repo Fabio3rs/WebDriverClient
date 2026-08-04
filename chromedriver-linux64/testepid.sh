bed30c65883193f57c9bb3d732494bf3f7a98a88
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2026-08-04 16:13:41 -0300

    Remove vendored local artifacts

--- chromedriver-linux64/testepid.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-
-./chromedriver &   # Run in background
-pid=$!          # Store the PID in 'pid' variable
-echo "PID: $pid"
-
-wait $pid       # Wait for the process to finish
-echo "Process $pid is finished"
